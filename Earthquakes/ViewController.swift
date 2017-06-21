0b774778be583f013052291ba49485137c9528bf
bradgayman <bgayman@apppartner.com>
2017-06-21 17:37:27 -0400

    Initial Commit

---
+++ Earthquakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Earthquakes
+//
+//  Created by App Partner on 6/21/17.
+//  Copyright © 2017 App Partner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
